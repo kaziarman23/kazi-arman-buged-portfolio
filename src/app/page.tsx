1e6a4483ab64732b1ede9ba976cde16d05629c39
Ethernal Knight <kaziarman@proton.me>
2024-11-03 14:34:34 +0600

    waiting for update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,3 +17,11 @@ const Home = () => {
 };
 
 export default Home;
+
+// website is working on localhost but it's not working when i deploy it
+// because react is updating and nextjs have the old default setting's stuff
+// that's why there were happening a dev-depandence conflict.
+// have to wait for the updated nextjs version
+// Time: 2.32 
+// PM Date: 3/11/24 
+// I am the real loser....
